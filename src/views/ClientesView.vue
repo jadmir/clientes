251a9cff4071adc172910f7ec98c0f124f989e54
JADMIR IGNACIO VICENTE SIFUENTES <jvicentes@utp.edu.pe>
2023-08-28 21:21:23 -0500

    json

--- src/views/ClientesView.vue
+++ src/views/ClientesView.vue
@@ -1,6 +1,17 @@
 <script setup>
+  import { onUnmounted } from 'vue'
+  import axios from 'axios';  
   import RouterLink from '../components/Ui/RouterLink.vue'
   import Heading from '../components/Ui/Heading.vue'
+
+  onUnmounted(() => {
+    axios.get('http://localhost:4000/clientes')
+      .then(({data}) => {
+        console.log(data)
+      })
+      .catch(error => console.log('Hubo un errro'))
+  })
+
   defineProps({
     titulo: {
       type: String
